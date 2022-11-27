ef4777ca954ce2cd703f7c8f5aa6afc91253f2d1
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-27 13:16:24 +0530

    new

--- src/components/customer/customer-list-results.js
+++ src/components/customer/customer-list-results.js
@@ -10,10 +10,17 @@ export const CustomerListResults = () => {
       <div className='row'>
         <div className='team-1'>
           <h1>SEREIL KILLER</h1>
+          <div className='text-center'>
+            <img src="https://ik.imagekit.io/cforcrypto/metaball/Dashboard/Cereal_Killers_1.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669535007586" alt=""/>
+            
+          </div>
           
         </div>
         <div className='team-2'>
           <h1>GOLDEN KNIGHTS</h1>
+          <div className='text-center'>
+            <img src="https://ik.imagekit.io/cforcrypto/metaball/Dashboard/Golden_Knights_1.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669535007319" alt=""/>
+          </div>
         </div>
         
       </div>
