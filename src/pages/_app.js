0cec21d2a4f161000c735153e7f7d977ef51fc94
noor alam <51040978+noorshanu@users.noreply.github.com>
2022-11-29 13:29:59 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -24,7 +24,7 @@ const App = (props) => {
     <CacheProvider value={emotionCache}>
       <Head>
         <title>
-          Material Kit Pro
+          METABALL|home
         </title>
         <meta
           name="viewport"
